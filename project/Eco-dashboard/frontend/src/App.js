59ab7bcd93cee059b9bebd420e1410907eb1e51b
jenu <jn.codistree@gmail.com>
2023-08-16 09:53:09 +0530

    Setup backend and frontend

---
+++ project/Eco-dashboard/frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Route from "./Routes";
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <Route />
+    </Fragment>
+  );
+}
+
+export default App;
