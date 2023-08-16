b21eb546b9aa25e75b1a1bc12b5993138b3ba150
jenu <jn.codistree@gmail.com>
2023-08-16 09:01:17 +0530

    Add pagination api

--- course/api-mangoose/index.js
+++ course/api-mangoose/index.js
@@ -23,10 +23,37 @@ app.post("/create", async (req, resp) => {
   resp.send(result);
 });
 
-//get
+//get http://localhost:4000/list?limit=2&page=2
+
 app.get("/list", async (req, resp) => {
-  let result = await product.find();
-  resp.send(result);
+  // let result = await product.find();
+  // resp.send(result);
+
+  const { page, limit } = req.query;
+  try {
+    const docs = await product
+      .find()
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .exec();
+    const totaldocs = await product.countDocuments();
+
+    const prevPage = parseInt(page) - 1;
+    const nextPage = parseInt(page) + 1;
+    resp.json({
+      totalPages: Math.ceil(totaldocs / limit),
+      currentPage: page,
+      totaldocs: totaldocs,
+      prevPage: prevPage,
+      nextPage: nextPage,
+      hasPrevPage: prevPage !== 0 ? true : false,
+      hasNextPage: Math.ceil(totaldocs / limit) == page ? false : true,
+      limit: limit,
+      docs,
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 //delete
@@ -56,7 +83,7 @@ app.get("/search/:key", async (req, resp) => {
 const Upload = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "uploads"); //uploads is file name
+      cb(null, "uploads"); //'uploads' is file name
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname + "-" + Date.now() + ".jpg");
